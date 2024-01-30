5b069e17c25b83c28b0fd0f7f09651086967e288
ccconac <qwerpoiu91@naver.com>
2024-01-31 03:01:11 +0900

    ✨ feat: implement reset button

--- script/counter.js
+++ script/counter.js
@@ -1,5 +1,6 @@
 const digit = document.getElementById('digit');
 const decreaseBtn = document.getElementById('decrease');
+const resetBtn = document.getElementById('reset');
 
 let count = 0;
 
@@ -9,4 +10,10 @@ const decrease = () => {
 
   if (count < 0) digit.style.color = 'red';
   if (count === 0) digit.style.color = 'black';
+};
+
+const reset = () => {
+  count = 0;
+  digit.textContent = 0;
+  digit.style.color = 'black';
 };
