8cd1c4fb683758c64116486271d3dfc4bd863324
ccconac <qwerpoiu91@naver.com>
2024-02-07 01:52:37 +0900

    ✨ feat: implement previous button

---
+++ scripts/app.mjs
@@ -0,0 +1,26 @@
+import { ANIMALS } from './animals.mjs';
+
+const animalImage = document.getElementById('img');
+const animalName = document.getElementById('animal-name');
+const animalType = document.getElementById('animal-type');
+const animalDescription = document.getElementById('description');
+
+const previousButton = document.getElementById('prev-btn');
+
+let currentAnimal = 0;
+
+const showAnimal = (current) => {
+  const animal = ANIMALS[current];
+
+  animalImage.src = animal.image;
+  animalName.textContent = animal.name;
+  animalType.textContent = animal.type;
+  animalDescription.textContent = animal.text;
+};
+
+previousButton.addEventListener('click', () => {
+  currentAnimal -= 1;
+  if (currentAnimal < 0) currentAnimal = ANIMALS.length - 1;
+
+  showAnimal(currentAnimal);
+});
