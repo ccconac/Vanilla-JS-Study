172754663afc28fa0aefb44ec13aec95f777eb70
ccconac <qwerpoiu91@naver.com>
2024-02-28 06:00:23 +0900

    :sparkles: feat: implement modal close button

--- week6/ccconac/app.js
+++ week6/ccconac/app.js
@@ -1,6 +1,11 @@
 const openButton = document.querySelector('.open-modal');
 const modal = document.querySelector('.modal-background');
+const closeButton = document.querySelector('.close-modal');
 
 openButton.addEventListener('click', () => {
   modal.classList.add('visible-modal');
+});
+
+closeButton.addEventListener('click', () => {
+  modal.classList.remove('visible-modal');
 });
