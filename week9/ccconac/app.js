b15c1a65c9c5531b687ad335c91e3be5e930b731
ccconac <qwerpoiu91@naver.com>
2024-03-20 04:22:19 +0900

    :sparkles: feat: implement switch button

--- week9/ccconac/app.js
+++ week9/ccconac/app.js
@@ -6,4 +6,15 @@ const hideLoadingIcon = () => {
   loading.classList.add('hide-loading');
 };
 
+const controlVideo = () => {
+  if (!switchButton.classList.contains('slide')) {
+    switchButton.classList.add('slide');
+    video.pause();
+  } else {
+    switchButton.classList.remove('slide');
+    video.play();
+  }
+};
+
 window.addEventListener('load', hideLoadingIcon);
+switchButton.addEventListener('click', controlVideo);
