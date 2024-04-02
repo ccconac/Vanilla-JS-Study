da6f720568d68d6e8772f5c98be49a9712391597
ccconac <qwerpoiu91@naver.com>
2024-04-02 18:20:00 +0900

    :sparkles: feat: change image upon click button

--- week11/ccconac/app.js
+++ week11/ccconac/app.js
@@ -2,6 +2,8 @@ const content = document.querySelector('.content-wrapper');
 const tabButton = document.querySelectorAll('.tab-button');
 const description = document.querySelectorAll('.content');
 
+let imageURL = document.getElementById('content-image');
+
 const handleButtonClick = (event) => {
   const id = event.target.dataset.id;
 
@@ -12,8 +14,14 @@ const handleButtonClick = (event) => {
 
     const element = document.getElementById(id);
     element.classList.add('display');
-
     element.childNodes[1].style.color = id;
+
+    if (id === 'green')
+      imageURL.src = 'https://i.pinimg.com/564x/95/c4/b5/95c4b5d8eea0e1bf7c3600fbb462cd13.jpg';
+    if (id === 'blue')
+      imageURL.src = 'https://i.pinimg.com/564x/b0/54/4b/b0544b2d4ea706c325c8dd5fbeb6de0f.jpg';
+    if (id === 'pink')
+      imageURL.src = 'https://i.pinimg.com/originals/f9/50/5f/f9505f91049a44c18874833bee79579b.jpg';
   }
 };
 
